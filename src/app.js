a5c4a34b84f1d800e6647cc6f0534945d811bec8
vepifanio <vepifanio1@gmail.com>
2024-01-22 21:43:22 -0300

    refactor: use repository and controller classes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
-const { randomUUID } = require('crypto');
 const { uploadImageFileMiddleware } = require('./upload-img-file-middleware');
-const { generateNewPdf } = require('./generate-new-pdf');
+const { InMemoryPDFsRepository } = require('./PDFsRepository');
+const { PDFsController } = require('./PDFsController');
 
 const app = express();
 app.use(cors());
@@ -11,54 +10,15 @@ app.use(express.static('public'));
 app.use(express.static('pdf'));
 app.use(express.json());
 
-const pdfs = [];
+const pdfsRepository = new InMemoryPDFsRepository();
+const pdfsController = new PDFsController(pdfsRepository);
 
-app.get('/pdfs', (req, res) => res.json({
-  pdfs,
-}));
+const index = pdfsController.index.bind(pdfsController);
+const create = pdfsController.create.bind(pdfsController);
+const getOne = pdfsController.getOne.bind(pdfsController);
 
-app.post('/pdfs/create', uploadImageFileMiddleware, (req, res) => {
-  try {
-    const { content } = req.body;
-
-    if (!content) {
-      return res.status(400).json({
-        error: "'content' field is missing",
-      });
-    }
-
-    const backgroundImageUrl = path.join('public', req.file.filename);
-
-    const pdf = {
-      id: randomUUID(),
-      content,
-      backgroundImageUrl,
-    };
-
-    pdfs.push(pdf);
-
-    return res.status(201).send();
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-});
-
-app.get('/pdfs/generate/:id', (req, res) => {
-  const { id } = req.params;
-
-  const pdf = pdfs.find((item) => item.id === id);
-
-  if (!pdf) {
-    return res.status(404).json({
-      error: 'No PDF found.',
-    });
-  }
-
-  const generatedPDFPath = generateNewPdf(pdf);
-
-  return res.json({
-    path: generatedPDFPath,
-  });
-});
+app.get('/pdfs', index);
+app.get('/pdfs/generate/:id', getOne);
+app.post('/pdfs/create', uploadImageFileMiddleware, create);
 
 module.exports = { app };
